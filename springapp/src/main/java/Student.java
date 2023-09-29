8b00ce19b29cf3cae29303e5bdb842294562b15d
Iamneo <techadmin@iamneo.ai>
2023-09-29 13:01:50 +0000

    Backup Commit

--- springapp/src/main/java/Student.java
+++ springapp/src/main/java/Student.java
@@ -31,6 +31,10 @@ public class Student {
     public void setLastName(String lastName){
         this.lastName=lastName;
     }
+    @Override
+    public String toString(){
+        return ""
+    }
 
 
 }
