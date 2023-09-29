1daf7bc1aa325cf04c2683de197a84b18fbc61b8
Iamneo <techadmin@iamneo.ai>
2023-09-29 12:56:19 +0000

    Backup Commit

--- springapp/src/main/java/Student.java
+++ springapp/src/main/java/Student.java
@@ -1,3 +1,20 @@
 public class Student {
-    private int stden
+    private int stdentId;
+    private String firstName;
+    private String lastName;
+    
+    public Student(int stdentId,String firstName, String lastName){
+        
+    }
+    public int getStudentId(){
+        return stdentId;
+    }
+    public void setStudentId(int stdentId){
+        this.stdentId=stdentId;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+
+
 }
